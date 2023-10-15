542f0f61db4dd34a19326663786e872915f5f13e
Joakim Falch <joakimfalch@outlook.com>
2023-10-15 17:39:34 +0200

    Added how to play screen

--- screens/HowToPlayScreen.js
+++ screens/HowToPlayScreen.js
@@ -1,24 +1,76 @@
 import React from 'react';
-import { View, Text,StyleSheet } from 'react-native';
+import { Text, ScrollView, StyleSheet } from 'react-native';
+import "../translation"
+import { useTranslation } from 'react-i18next';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 function HowToPlayScreen() {
+    const { t } = useTranslation();
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.header}>How To Play</Text>
-        </View>
+        <ScrollView style={styles.container}>
+            <Text style={styles.header}>{ t('how_title') }</Text>
+            <Text style={styles.text}>
+                {t('how_intro')}
+            </Text>
+            <Text style={styles.subHeader}>{t('how_rules')}</Text>
+            <Text style={styles.text}>
+                {t('how_rules_1')}
+            </Text>
+            <Text style={styles.text}>
+                {t('how_rules_2')}
+            </Text>
+            <Text style={styles.text}>
+                {t('how_rules_3')}
+            </Text>
+            <Text style={styles.text}>
+                <FontAwesome5 name="eraser"  size={24} color="#4e9bff"/>
+                {t('how_eraser')}
+            </Text>
+            <Text style={styles.text}>
+                <FontAwesome5 name="font-awesome-flag"  size={24} color="#4e9bff"/>
+                {t('how_flag')}
+            </Text>
+            <Text style={styles.subHeader}>{t('help_header')}</Text>
+            <Text style={styles.text}>
+                {t('help_1')}
+            </Text>
+            <Text style={styles.text}>
+                {t('help_2')}
+            </Text>
+            <Text style={styles.text}>
+                {t('help_3')}
+            </Text>
+            <Text style={styles.text}>
+                {t('help_4')}
+            </Text>
+            <Text style={styles.text}>
+                {t('help_5')}
+            </Text>
+        </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        padding: 20,
         backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
     },
     header: {
         fontSize: 32,
         marginBottom: 20,
+        textAlign: 'center',
+    },
+    subHeader: {
+        fontSize: 24,
+        marginTop: 20,
+        marginBottom: 10,
+    },
+    text: {
+        fontSize: 16,
+        textAlign: 'left',
+        marginBottom: 10,
     }
 });
 
--- translation/resources/no.js
+++ translation/resources/no.js
@@ -14,5 +14,20 @@ export default {
   "cancel": "Avbryt",
   "correct": "Løsningen er riktig, godt jobbet!",
   "incorrect": "Løsningen er ikke riktig :(",
-  "generate": "Lag nytt brett"
+  "generate": "Lag nytt brett",
+  "how_title": "Hvordan Spille Sudoku",
+  "how_intro": "Sudoku er en logikkbasert tallplasseringspuslespill. Målet er å fylle en 9×9-rutenett med sifre slik at hver kolonne, hver rad og hver av de ni 3×3-undernettene som utgjør rutenettet inneholder alle sifrene fra 1 til 9.",
+  "how_rules": "Regler:",
+  "how_rules_1": "1. Hver rad må inneholde tallene 1 til 9, uten gjentakelse.",
+  "how_rules_2": "2. Hver kolonne må også inneholde tallene 1 til 9, igjen uten å gjenta.",
+  "how_rules_3": "3. De 3×3 rutenettene som danner det større rutenettet må også inneholde tallene 1 til 9 uten gjentakelse.",
+  "how_eraser": " Viskelær-ikonet brukes til å fjerne tallet som er i en rute.",
+  "how_flag": " Flag-ikonet brukes til å markere en rute dersom du er usikker på om tallet i ruten er riktig.",
+  "help_header": "Trinn for å Løse Sudoku:",
+  "help_1": "1. Start med å fokusere på rader, kolonner og 3×3-ruter som har flere tall allerede fylt inn.",
+  "help_2": "2. Bruk logikk for å utlede mulighetene for gjenværende ruter.",
+  "help_3": "3. Bruk en elimineringsprosess for å begrense mulighetene.",
+  "help_4": "4. Vær tålmodig og stå på! Noen brett kan være vanskelige.",
+  "help_5": "Lykke til!"
+
 };
